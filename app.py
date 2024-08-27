c597e18b8c7ac6687eac971c66577eb1fedd0661
judeleonard <judeleonard86@gmail.com>
2024-08-27 09:54:55 +0100

    sync dev branch to main

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.markdown("# Oil Prices Scraping and Analytics")
 
+st.write("For information about getting data, head over to the data page in the navigation bar and for analytics also. "
+         "NB: This page is merely and sample and does not fully represent how the final page will appear however,"
+         "all features in this demo will be present in the final application.")
 
 pg = st. navigation([
     st.Page('./pages/Data.py', title='Data', icon=":material/favorite:"),
--- pages/Data.py
+++ pages/Data.py
@@ -1,11 +1,18 @@
 import streamlit as st
-from utils.module import data_loader
+import pandas as pd
+from utils.module import extract_percentage, fetch_latest_data
 
 st.markdown("## Data Sourcing Page")
 st.write("This pages is where the data scraping happens, All we need is a button that load the data from a specific "
          "webpage")
 
+df = pd.read_csv('./data/oil_prices_filtered.csv')
+df = df.drop('Unnamed: 0', axis=1)
+df = df.drop([0], axis = 0).reset_index(drop=True) 
 
-df = data_loader()
+df['% Change'] = df['% Change'].apply(extract_percentage)
+st.dataframe(df)
 
-st.dataframe(df.head(5))
+if st.button("Update data from web"):
+    fetch_latest_data()
+    st.success("Data successfully updated")
